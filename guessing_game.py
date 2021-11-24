abad0c9f1e3eaa748b5afe686fd8374fc43fa667
Matteo Floirat <matteo.floirat@edu.devinci.fr>
2021-11-24 11:09:36 +0100

    Guessing game

--- guessing_game.py
+++ guessing_game.py
@@ -2,16 +2,21 @@
 import random
 def guess_loop ():
     number_to_guess = random.randint(1, 100)
+    try_count = 0
     print("I have in mind a number in between 1 and 100, can you findit?")
     while True:
             try:
                 guess = int(input())
                 if guess > number_to_guess:
                     print("The number to guess is lower")
+                    try_count += 1
                 elif guess < number_to_guess:
                     print("The number to guess is higher")
+                    try_count += 1
                 else:
                     print("You just found the number , it was indeed",guess)
+                    print("With",try_count)
+                    print("try")
                     return
             except ValueError as err:
                 print("Invalid input , please enter an integer")
